6b9302f5a41a53e58d935d53ba62d433dab6b5ca
priyanshu <negip4484@gmail.com>
2026-06-06 20:46:44 +0530

    basic setup completed

---
+++ android/app/src/main/kotlin/com/example/sync_rescue/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sync_rescue
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sync_rescue/firebase_options.dart';
+import 'package:sync_rescue/pages/hone_paage/home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'synch rescue',
+      theme: ThemeData(textTheme: GoogleFonts.latoTextTheme()),
+      home: const HomePage(),
+    );
+  }
+}
