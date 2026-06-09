90f03dafc214880cbf9915206d5314f96aee0a57
priyanshu <negip4484@gmail.com>
2026-06-09 21:54:34 +0530

    view model completed

--- lib/features/auth/view_models/auth_view_model.dart
+++ lib/features/auth/view_models/auth_view_model.dart
@@ -1,7 +1,94 @@
 import 'package:flutter/foundation.dart';
+import 'package:sync_rescue/features/auth/models/user_model.dart';
 import 'package:sync_rescue/features/auth/services/firebase_auth_services.dart';
 
 class AuthViewModel extends ChangeNotifier {
-  final FirebaseAuthServices _firebaseAuthServices = FirebaseAuthServices();
-  
+  final FirebaseAuthServices _authServices = FirebaseAuthServices();
+
+  UserModel? _currentUser;
+  bool _isLoading = false;
+  String _errorMessage = "";
+
+  UserModel? get currentUser => _currentUser;
+  bool get isLoading => _isLoading;
+  String get errorMessage => _errorMessage;
+
+  Future<bool> signUp(
+    String email,
+    String password,
+    String name,
+    String phoneNo,
+  ) async {
+    try {
+      _isLoading = true;
+      _errorMessage = "";
+      notifyListeners();
+
+      UserModel? returnedUser = await _authServices.signUpWithEmail(
+        email,
+        password,
+        name,
+        phoneNo,
+      );
+
+      if (returnedUser != null) {
+        _currentUser = returnedUser;
+        return true;
+      } else {
+        _errorMessage = "Something went wrong. User is null.";
+        return false;
+      }
+    } catch (e) {
+      _errorMessage = e.toString();
+      return false;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<bool> login(String email, String password) async {
+    try {
+      _isLoading = true;
+      _errorMessage = "";
+      notifyListeners();
+
+      UserModel? returndUser = await _authServices.loginWithEmail(
+        email,
+        password,
+      );
+
+      if (returndUser != null) {
+        _currentUser = returndUser;
+        return true;
+      } else {
+        _errorMessage = "Something went wrong. user is null";
+        return false;
+      }
+    } catch (e) {
+      _errorMessage = e.toString();
+      return false;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<bool> logout() async {
+    try {
+      _isLoading = true;
+      _errorMessage = "";
+      notifyListeners();
+
+      await _authServices.logout();
+      _currentUser = null;
+      return true;
+    } catch (e) {
+      _errorMessage = e.toString();
+      return false;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
 }
