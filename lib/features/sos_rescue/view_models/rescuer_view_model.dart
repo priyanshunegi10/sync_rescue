5969b0687a515771fd5299220b16dcb219630a3a
priyanshu <negip4484@gmail.com>
2026-06-17 03:27:17 +0530

    RescuerViewModel completed

---
+++ lib/features/sos_rescue/view_models/rescuer_view_model.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:sync_rescue/core/errors/app_exception.dart';
+import 'package:sync_rescue/features/sos_rescue/models/sos_request_model.dart';
+import 'package:sync_rescue/features/sos_rescue/services/firestore_sos_services.dart';
+
+class RescuerViewModel extends ChangeNotifier {
+  final FirestoreSosServices _db = FirestoreSosServices();
+
+  String _errorMessage = "";
+  bool _isLoading = false;
+  List<SosRequestModel> _pendingEmergencies = [];
+
+  String get errorMessage => _errorMessage;
+  bool get isLoading => _isLoading;
+  List<SosRequestModel> get fetchAllEmergencies => _pendingEmergencies;
+
+  Future<bool> fetchPendingEmergencies() async {
+    try {
+      _isLoading = true;
+      _errorMessage = "";
+      notifyListeners();
+
+      final emergencies = await _db.getPendingEmergencies();
+
+      _pendingEmergencies = emergencies;
+
+      return true;
+    } on DatabaseException catch (e) {
+      _errorMessage = e.message;
+      return false;
+    } catch (e) {
+      _errorMessage = "An unexpected error occurred: $e";
+      return false;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<bool> acceptRequest(String requestId) async {
+    try {
+      _isLoading = true;
+      _errorMessage = "";
+      notifyListeners();
+      await _db.acceptSosRequest(requestId);
+
+      _pendingEmergencies.removeWhere((req) => req.requestId == requestId);
+      return true;
+    } on DatabaseException catch (e) {
+      _errorMessage = e.message;
+      return false;
+    } catch (e) {
+      _errorMessage = "An unexpected error occurred: $e";
+      return false;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
