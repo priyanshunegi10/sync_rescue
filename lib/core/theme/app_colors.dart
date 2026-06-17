8bd677ec4ae5fde69608acd6190b4d6c769d8a83
priyanshu <negip4484@gmail.com>
2026-06-18 01:39:15 +0530

    home page UI completed

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  // login page colors
   static const Color blackColor = Colors.black;
   static const Color whiteColor = Colors.white;
   static const Color redColor = Colors.red;
   static Color greyColorsShade600 = Colors.grey.shade600;
   static Color greyColorsShade300 = Colors.grey.shade300;
+
+  // home page colors
+
+  static const Color backgroundColor = Color(0xff050B0F);
+  static const Color green = Color(0xff5DC067);
+  static const Color darkblue = Color(0xff111D2C);
+  static const Color lightblue = Color(0xff1B283F);
+  static Color lightWhite = Colors.white60;
 }
