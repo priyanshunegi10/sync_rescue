3849380a5e670b17a3adb6a92b7b71a562ad6f08
priyanshu <negip4484@gmail.com>
2026-06-24 01:02:15 +0530

    local storage service completed

---
+++ lib/core/services/local_storage_service.dart
@@ -0,0 +1,23 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocalStorageService {
+  static const String _activeMissionKey = 'ACTIVE_MISSION_ID';
+
+  static Future<bool> saveActiveMission(String requestId) async {
+    final pref = await SharedPreferences.getInstance();
+
+    return await pref.setString(_activeMissionKey, requestId);
+  }
+
+  static Future<String?> getActiveMission() async {
+    final pref = await SharedPreferences.getInstance();
+
+    return pref.getString(_activeMissionKey);
+  }
+
+  static Future<bool> clearActiveMission() async {
+    final pref = await SharedPreferences.getInstance();
+
+    return await pref.remove(_activeMissionKey);
+  }
+}
